57c9d72958552fd89b06c2902dd8f64dacc21efc
KreoX <soldimksol@gmail.com>
2022-08-21 13:55:22 +0300

    Ex3

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -2,20 +2,14 @@
 // Найдите сумму отрицательных и положительных элементов массива.
 // Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29, 
 // сумма отрицательных равна -20.
-void RandomArray(int[] arr)
-{
-    for (int i = 0; i < arr.Length; i++)
-    {
-        arr[i] = new Random().Next(-9, 10);
-    }
-}
+
 
 int[] array = new int[12];
 for (int i = 0; i < array.Length; i++)
 {
     array[i] = new Random().Next(-9, 10);
 }
-RandomArray(array);
+
 Console.WriteLine($"[{String.Join(", ", array)}]");
 int sumPos = 0;
 int sumNeg = 0;
---
+++ Ex32/Program.cs
@@ -0,0 +1,32 @@
+﻿int [] RandomArray(int length, int min, int max)
+{
+    int [] array= new int [length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+    }
+    return array;
+}
+
+int [] ReverseArray(int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+{
+    array[i]= -array[i];
+}
+return array;
+}
+
+
+
+Console.Write("Введите длину массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите минимальное значение элемента массива: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное значение элемента массива: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] array = RandomArray(length, min, max);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+int [] RevArray= ReverseArray(array);
+Console.WriteLine($"[{String.Join(", ", RevArray)}]");
